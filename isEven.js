07056dc1165537ae77f0dc04191fa49a97195e4b
Adam Turner <hello@adamrichardturner.dev>
2021-06-24 17:39:34 +0100

    Created isEven function that checks if an inputted value is a positive, whole number that is even - returning a boolean true or false depending on whether it is even - uses recursion

---
+++ isEven.js
@@ -0,0 +1,20 @@
+// Define a recursive function isEven corresponding to this description:
+// 1. Zero is even.
+// 2. One is odd.
+// 3. For any other number N, its evenness is the same as N - 2
+// Function should accept a single parameter, a positive whole number 
+// and return a Boolean value
+
+const isEven = num => {
+    if(num < 0) {
+        return false;
+    } else if(num === 0) {
+        return true;
+    } else if (num === 1) {
+        return false;
+    } else {
+        return isEven(num - 2);
+    }
+}
+
+console.log(isEven(-2));
